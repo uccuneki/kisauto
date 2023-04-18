ae189f896f13813ac6f7526fdef1157a520b45ce
uccuneki <131049418+uccuneki@users.noreply.github.com>
2023-04-18 15:58:43 +0200

    forgatas mindket szervoval

--- rotate.py
+++ rotate.py
@@ -7,23 +7,27 @@ from inputs import get_gamepad
 from gpiozero import Servo
 
 factory = PiGPIOFactory()
-servo = Servo(18, min_pulse_width=0.0005, max_pulse_width=0.0025, pin_factory=factory)
-servo.min()
+servo1 = Servo(18, min_pulse_width=0.0005, max_pulse_width=0.0025, pin_factory=factory)
+servo2 = Servo(19, min_pulse_width=0.0005, max_pulse_width=0.0025, pin_factory=factory)
 
 BTN_A = "BTN_SOUTH"  # button A
 STATE = -1
 
+servo1.mid()
+servo2.mid()
 
 def rotate():
     global STATE
     if STATE == -1:
         print("Rotate: max")
-        servo.max()
+        servo1.min()
+        servo2.max()
         STATE = 1
         sleep(1)
     else:
         print("Rotate: min")
-        servo.min()
+        servo1.max()
+        servo2.min()
         STATE = -1
         sleep(1)
         
